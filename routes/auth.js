3836c525f5a0aca36c6bf17b2bbd5d592df5d8bd
Peter Neville <neville.peter.2000@gmail.com>
2022-11-06 02:03:26 -0500

    some changes

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ import { register, login } from '../controllers/auth.js';
 const router = express.Router();
 
 router.post('/auth/login', login);
+router.get('/auth/check', login);
 router.post('/auth/register', register);
 
 export default router;
